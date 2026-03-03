7183da4ec34bcda487861e22ea9da66ee795c92b
tuningMaster <tuningMaster@gmail.com>
2026-03-04 01:01:14 +0800

    增加test case

--- scorpion-test/src/test/java/com/scorpion/core/test/core/scan/ScanTest.java
+++ scorpion-test/src/test/java/com/scorpion/core/test/core/scan/ScanTest.java
@@ -1,12 +1,15 @@
 package com.scorpion.core.test.core.scan;
 
 import com.alibaba.fastjson.TypeReference;
+import com.google.common.collect.Lists;
 import com.scorpion.core.processor.ResourceProcessor;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class ScanTest {
 
@@ -15,6 +18,6 @@ public class ScanTest {
         Set<String> strings = ResourceProcessor.load(ScanTest.class.getClassLoader(), ScanConstants.RESOURCE_FILE_1,
                 new TypeReference<Set<String>>() {});
         List<String> lists = strings.stream().sorted().collect(Collectors.toList());
-        System.out.println(lists);
+        assertEquals(Lists.newArrayList("com.scorpion.core.test.core.scan.clazz.TestScanClass"), lists);
     }
 }
